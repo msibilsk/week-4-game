5f050cd8521b18a75952fd1c5dc895e2cad59d1e
Mary Willis <msibilsk@gmail.com>
2016-11-14 19:58:48 -0600

    logic for selecting characters, attack/counter attack and display for whether win/lose is complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,20 +8,20 @@ $(document).ready(function() {
         }
     };
 
-    var yoda = new Character(100, 15, 25);
-    var han = new Character(100, 20, 50);
-    var vader = new Character(200, 25, 75);
-    var trooper = new Character(75, 10, 20);
+    var yoda = new Character(90, 10, 20);
+    var han = new Character(85, 10, 15);
+    var vader = new Character(200, 15, 20);
+    var trooper = new Character(75, 8, 10);
 
     var characterMap = {
         yoda: yoda,
         han: han,
         vader: vader,
         trooper: trooper,
-    }
+    };
 
-    var player;
-    var opponent;
+    var player = undefined;
+    var opponent = undefined;
     var playerName;
     var opponentName;
 
@@ -33,34 +33,54 @@ $(document).ready(function() {
 
     showHP();
 
-    var selectOpponent = function() {
-        $("h1").html("Select Your Opponent");
-        $(".character").css("border", "5px solid red");
-        $("#player .character").css("border", "none");
-        $(".character").on("click", function() {
+    $(".character").on("click", function() {
+        if (player === undefined) {
+            playerName = $(this).attr("id");
+            player = characterMap[playerName];
+            $(this).prependTo("#player");
+            $("#playerSelection").html("Select Your Opponent");
+            $(".character").css("border", "5px solid red");
+            $("#player .character").css("border", "none");
+        } else if (opponent === undefined) {
             opponentName = $(this).attr("id");
             opponent = characterMap[opponentName];
-            console.log(opponent.healthPoints);
             $("button").css("visibility", "visible");
-            $(this).appendTo("#opponent");
-            $("#characterGallery").empty();
-        });
-    }
-
-    $(".character").on("click", function() {
-        playerName = $(this).attr("id");
-        player = characterMap[playerName];
-        console.log(player.healthPoints);
-        $(this).appendTo("#player");
-        selectOpponent();
+            $(this).prependTo("#opponent");
+            $("#opponent .character").css("border", "none");
+            $("#instructions").empty();
+        }
     });
+
+    // var reset = function (){
+    //     $("#opponent").remove("#" + opponentName);
+    //     $("#player").remove("#" + playerName);
+    //     for (var key in characterMap) {
+    //         $("#characterGallery").append($("<div id='" + key + "'>"));
+    //     }
+    // };
 
     $("button").on("click", function() {
         opponent.healthPoints -= player.attackPower;
+        $("#attackDamage").html("You attacked for " + player.attackPower + " damage.");
+        player.attackPower = Math.floor(player.attackPower * 1.5);
         showHP();
-        player.healthPoints -= opponent.counterAttack;
-        console.log(player.healthPoints);
-        showHP();
+        if (opponent.healthPoints > 0) {
+            player.healthPoints -= opponent.counterAttack;
+            $("#counterAttackDamage").html(opponentName.toUpperCase() + " counter attacked for " + opponent.counterAttack + " damage.");
+            showHP();
+            if (player.healthPoints < 0) {
+                $("#announce").html("NoooOOOOooooOOO! You Lose")
+            }
+        } else {
+            if ( $('#characterGallery').children().length == 1 ) {
+                $("#announce").html("The Force Is Strong With You...You Win!");
+            } else {
+                $("#instructions").html("Search your feelings and select another opponet!")
+                $("#" + opponentName).remove();
+                $("#counterAttackDamage").html("");
+                opponent = undefined;
+            }
+        }
     });
-    
+
 });
